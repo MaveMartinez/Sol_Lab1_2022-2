6ff3c4ce86c18a48142a44cb9b0a8fe3f56c6960
alulab14 <alulab14@V20524>
2024-03-21 20:37:46 -0500

    ca,mbio6

--- Lab1_2022-2/Lab1_2022-2.cpp
+++ Lab1_2022-2/Lab1_2022-2.cpp
@@ -6,11 +6,11 @@ const int MAX = 16;
 //declaracion del using namespace
 //declaracion del using namespace
 using namespace std;
-
+//cambio4
 void read_data(int *ptr_n, int coded_word_numbers[], char coded_word[]);
 void show_decoded_word(int n, int coded_word_numbers[], char coded_word[]);
 void sort(int n, int coded_word_numbers[], char coded_word[]);
-
+//cambio6
 int main()
 {
     int n;
