541ac14d1014c4a9399dc5e66ac0f66d8b66ed25
alulab14 <alulab14@V20524>
2024-03-21 21:41:11 -0500

    sin comentarios

--- Lab1_2022-2/Lab1_2022-2.cpp
+++ Lab1_2022-2/Lab1_2022-2.cpp
@@ -1,18 +1,13 @@
 // Lab1_2022-2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-//cambio de prueba 20
+
 #include <iostream>
 const int MAX = 16;
 
 using namespace std;
-//cambio4
-//otro cambio
+
 void read_data(int *ptr_n, int coded_word_numbers[], char coded_word[]);
 void show_decoded_word(int n, int coded_word_numbers[], char coded_word[]);
 void sort(int n, int coded_word_numbers[], char coded_word[]);
-//cambio6
-//CAMBIAZOOOOOOOOOOOO
-//OTRO CAMBIO AAAAAAAAAAAA
 
 int main()
 {
