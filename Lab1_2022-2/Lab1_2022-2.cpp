05564d5110c54f5fa9fda6695ba7fd1abfe8923e
Edwardvald <a20200655@pucp.edu.pe>
2024-03-21 20:42:56 -0500

    OTRO CAMBIO

--- Lab1_2022-2/Lab1_2022-2.cpp
+++ Lab1_2022-2/Lab1_2022-2.cpp
@@ -12,6 +12,7 @@ void show_decoded_word(int n, int coded_word_numbers[], char coded_word[]);
 void sort(int n, int coded_word_numbers[], char coded_word[]);
 //cambio6
 //CAMBIAZOOOOOOOOOOOO
+//OTRO
 
 int main()
 {
